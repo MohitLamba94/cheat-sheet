d273ff0de98f5a6eaf324050f69cea3010eae790
Mohit Lamba <60866304+MohitLamba94@users.noreply.github.com>
2025-01-16 17:01:54 +0530

    Debugging via system profiler

--- vs-code/terminal_debug.py
+++ vs-code/terminal_debug.py
@@ -11,3 +11,18 @@ p: to print the value of an expression in the current context
 pp: pretty print
 where
 return
+
+# How to set the profiler
+import sys
+import inspect
+def profile_function_calls(frame, event, arg):
+    if event == "call":
+        func_name = frame.f_code.co_name
+        file_path = frame.f_code.co_filename
+        if "/exp/root" in file_path:
+            print(f"--><-- Calling {func_name} in {file_path}")
+    return profile_function_calls
+
+# Set the profiling function
+sys.setprofile(profile_function_calls)
+
